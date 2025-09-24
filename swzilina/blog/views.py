7c94ba996c8a7fa3253b88f378e8434cab184c1f
Patrik Behul <behulpatrik@gmail.com>
2025-09-24 15:50:24 +0200

    Before Starting Again.

--- swzilina/blog/views.py
+++ swzilina/blog/views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+# Data For Themes In Blog Page.
+
+themes = [
+    {
+        "theme": "Front Lever",
+        "slug": "frontlever",
+        "page": "frontlever.html"
+    },
+
+    {
+        "theme": "Stojka",
+        "slug": "handstand",
+        "page": "handstand.html"
+    }
+]
+
+# Functions For Render Content On Pages.
 
 def home_page(request):
     return render(request, "blog/index.html")
 
 def blog(request):
-    return render(request, "blog/blog.html")
+    return render(request, "blog/blog.html", {
+        "themes": themes
+    })
 
-def blog_theme(request):
-    return render(request, "blog/blog_theme.html")
+def blog_theme(request, slug):
+    identified_theme = next(theme for theme in themes if theme["slug"] == slug)
+    print(identified_theme)
+    return render(request, "blog/blog_theme.html", {
+        "identified_theme": identified_theme
+    })
 
 # def frontlever(request):
 #     return render(request, "blog/frontlever.html")
