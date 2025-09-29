1779f31c24a54117d02496243268fdd4f2eeb220
Patrik Behul <behulpatrik@gmail.com>
2025-09-29 14:35:34 +0200

    Working On Home Page.

--- swzilina/blog/urls.py
+++ swzilina/blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage_url'),
+    path('login', views.login, name='login_url'),
+    path('registration', views.registration, name='registration_url'),
 ]
--- swzilina/blog/views.py
+++ swzilina/blog/views.py
@@ -3,3 +3,9 @@ from django.http import HttpResponse
 
 def homepage(request):
     return render(request, "blog/homepage.html")
+
+def login(request):
+    return render(request, "blog/login.html")
+
+def registration(request):
+    return render(request, "blog/registration.html")
