3e5cd53d75679209ee736d6841fb4277413a8796
Patrik Behul <behulpatrik@gmail.com>
2025-10-02 18:54:50 +0200

    Creating Model For Users Table.

--- swzilina/blog/models.py
+++ swzilina/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    first_name = models.CharField(verbose_name="First Name", max_length=50, null=False)
+    last_name = models.CharField(verbose_name="Last Name", max_length=50, null=False)
+    email_address = models.CharField(verbose_name="E-mail Address", max_length=50, null=False)
+    phone_number = models.CharField(verbose_name="Phone Number", max_length=50, null=True)
+    password = models.CharField(verbose_name="Password", max_length=255, null=False)
+    role = models.CharField(verbose_name="Role", choices=("user", "admin"), default="user", null=False)
+
+    def __str__(self):
+        return f"{self.role}: {self.first_name} {self.last_name}"
