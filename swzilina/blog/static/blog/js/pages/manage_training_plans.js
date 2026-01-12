0035b910d7591fa3334c82d45577a440acacbed3
Patrik Behul <behulpatrik@gmail.com>
2026-01-12 20:50:27 +0100

    Added Progress Bar, Sets And Reps Functionalities On The Manage Training Plan Page.

--- swzilina/blog/static/blog/js/pages/manage_training_plans.js
+++ swzilina/blog/static/blog/js/pages/manage_training_plans.js
@@ -15,6 +15,27 @@ document.addEventListener("DOMContentLoaded", function() {
         exercise_title.textContent = exercise_name
         exercise.appendChild(exercise_title)
 
+        // Creates Reps Title
+        const exercise_reps_title = document.createElement("p")
+        exercise_reps_title.textContent = "Počet opakovaní"
+        // exercise.appendChild(exercise_reps_title)
+
+        // Creates Periods (Sets & Reps) Container
+        const periods_container = document.createElement("div")
+        periods_container.classList.add("periods_container")
+        exercise.appendChild(periods_container)
+
+        // Creates Exercise Reps Container
+        const exercise_reps_container = document.createElement("div")
+        exercise_reps_container.classList.add("exercise_reps_container")
+        periods_container.appendChild(exercise_reps_container)
+
+        // Creates Exercise Reps Decrease Button
+        const decrease_exercise_reps = document.createElement("button")
+        decrease_exercise_reps.classList.add("decrease_exercise_reps")
+        decrease_exercise_reps.innerHTML = "<i class='fa-solid fa-minus'></i>" // https://fontawesome.com/icons/minus
+        exercise_reps_container.appendChild(decrease_exercise_reps)
+
         // Creates Exercise Reps Input
         const exercise_reps = document.createElement("input")
         exercise_reps.classList.add("reps")
@@ -22,7 +43,42 @@ document.addEventListener("DOMContentLoaded", function() {
         exercise_reps.value = 0
         exercise_reps.min = 0
         exercise_reps.max = 100
-        exercise.appendChild(exercise_reps)
+        exercise_reps_container.appendChild(exercise_reps)
+
+        // Creates Exercise Reps Increase Button
+        const increase_exercise_reps = document.createElement("button")
+        increase_exercise_reps.classList.add("increase_exercise_reps")
+        increase_exercise_reps.innerHTML = "<i class='fa-solid fa-plus'></i>" // https://fontawesome.com/icons/plus
+        exercise_reps_container.appendChild(increase_exercise_reps)
+
+        // Adds Decrease Exercise Reps Functionality
+        decrease_exercise_reps.addEventListener("click", function() {
+            let reps = parseInt(exercise_reps.value) // Gets Current Reps Amount In Number Format
+            reps -= 1 // Decreases Reps Amount By 1
+
+            if(reps < 0) return // Do Nothing
+            exercise_reps.value = reps // Updates Exercise Reps Amount
+        })
+        
+        // Adds Increase Exercise Reps Functionality
+        increase_exercise_reps.addEventListener("click", function() {
+            let reps = parseInt(exercise_reps.value) // Gets Current Reps Amount In Number Format
+            reps += 1 // Increases Reps Amount By 1
+
+            if(reps > 100) return // Do Nothing
+            exercise_reps.value = reps // Updates Exercise Reps Amount
+        })
+
+        // Creates Exercise Sets Container
+        const exercise_sets_container = document.createElement("div")
+        exercise_sets_container.classList.add("exercise_sets_container")
+        periods_container.appendChild(exercise_sets_container)
+
+        // Creates Exercise Sets Decrease Button
+        const decrease_exercise_sets = document.createElement("button")
+        decrease_exercise_sets.classList.add("decrease_exercise_sets")
+        decrease_exercise_sets.innerHTML = "<i class='fa-solid fa-minus'></i>" // https://fontawesome.com/icons/minus
+        exercise_sets_container.appendChild(decrease_exercise_sets)
 
         // Creates Exercise Sets Input
         const exercise_sets = document.createElement("input")
@@ -31,14 +87,106 @@ document.addEventListener("DOMContentLoaded", function() {
         exercise_sets.value = 1
         exercise_sets.min = 1
         exercise_sets.max = 100
-        exercise.appendChild(exercise_sets)
+        exercise_sets_container.appendChild(exercise_sets)
+
+        // Creates Exercise Sets Increase Button
+        const increase_exercise_sets = document.createElement("button")
+        increase_exercise_sets.classList.add("increase_exercise_sets")
+        increase_exercise_sets.innerHTML = "<i class='fa-solid fa-plus'></i>" // https://fontawesome.com/icons/plus
+        exercise_sets_container.appendChild(increase_exercise_sets)
+
+        // Adds Decrease Exercise Sets Functionality
+        decrease_exercise_sets.addEventListener("click", function() {
+            let sets = parseInt(exercise_sets.value) // Gets Current Sets Amount In Number Format
+            sets -= 1 // Decreases Sets Amount By 1
+
+            if(sets < 1) return // Do Nothing
+            exercise_sets.value = sets // Updates Exercise Sets Amount
+        })
+        
+        // Adds Increase Exercise Sets Functionality
+        increase_exercise_sets.addEventListener("click", function() {
+            let sets = parseInt(exercise_sets.value) // Gets Current Sets Amount In Number Format
+            sets += 1 // Increases Sets Amount By 1
+
+            if(sets > 100) return // Do Nothing
+            exercise_sets.value = sets // Updates Exercise Sets Amount
+        })
+
+        updateProgressBar()
+
+        const exercises = document.querySelectorAll(".training_plan_container .training_plan .exercise") // Gets All Exercises
+        changeExercises(exercises.length - 1) // Shows The Last Added Exercise
+        // updateReps(exercises.length - 1) // Calls Update Reps Function On Every Added Exercise
+    }
+
+    // function updateReps(exercise_index) {
+        
+    // }
+
+    // Function For Add Bar To The Progress Bar
+    function updateProgressBar() {
+        const training_plan = document.querySelector(".training_plan_container .training_plan") // Gets Training Plan
+        const progress_bar = document.querySelector(".training_plan_container .training_plan .progress_bar") // Gets Progress Bar
+
+        training_plan.appendChild(progress_bar) // Appends Progress Bar To The End Of The Training Plan
+
+        // Creates Bar
+        const bar = document.createElement("div")
+        bar.classList.add("bar")
+        progress_bar.appendChild(bar)
+
+        // Adds Click Event Listener To The New Bar In The Progress Bar
+        bar.addEventListener("click", function(event) {
+            chooseExerciseWithProgressBar(event.target)
+        })
+    }
+
+    // Function For Choose Exercise With Bars In The Progress Bar
+    function chooseExerciseWithProgressBar(clicked_bar) {
+        const bars = document.querySelectorAll(".training_plan_container .training_plan .progress_bar .bar") // Gets All Bars From The Progress Bar
+
+        // Removes Active Class From Every Bar In The Progress Bar
+        bars.forEach(function(one_bar) {
+            one_bar.classList.remove("active")
+        })
+
+        clicked_bar.classList.add("active") // Adds Active Class To The Clicked Bar
+
+        const clicked_bar_index = [...bars].indexOf(clicked_bar) // Gets Index Of Clicked Bar
+        changeExercises(clicked_bar_index) // Shows Exercise With Equal Index
+    }
+
+    // Function For Change Exercises In Training Plan
+    function changeExercises(exercise_index) {
+        const exercises = document.querySelectorAll(".training_plan .exercise") // Gets All Exercises
+
+        add_exercise.classList.remove("active") // Hides Add Exercise Drop Zone
+
+        // Hides Every Exercise
+        exercises.forEach(function(one_exercise) {
+            one_exercise.classList.remove("active")
+        })
+
+        // exercises[exercises.length - 1].classList.add("active") // Shows The Last Exercise
+        // exercises[0].classList.add("active") // Shows The Last Added Exercise
+        exercises[exercise_index].classList.add("active") // Shows Exercise With The Chosen Index
+
+        const bars = document.querySelectorAll(".training_plan_container .training_plan .progress_bar .bar") // Gets All Bars From The Progress Bar
+
+        // Removes Active Class From Every Bar In The Progress Bar
+        bars.forEach(function(one_bar) {
+            one_bar.classList.remove("active")
+        })
+
+        bars[exercise_index].classList.add("active") // Adds Active Class To The Bar Of The Current Active Exercise
     }
 
     // Exercises
-    const exercises = document.querySelectorAll(".exercises .exercise") // Gets All Exercises
+    const exercises_selection = document.querySelectorAll(".exercises .exercise") // Gets All Exercises From Selection
     let dragged_exercise = null // Stores Dragged Exercise
 
-    exercises.forEach(function(one_exercise) {
+    exercises_selection.forEach(function(one_exercise) {
         // Drag Start
         one_exercise.addEventListener("dragstart", function() {
             dragged_exercise = one_exercise // Saves Dragged Exercise
