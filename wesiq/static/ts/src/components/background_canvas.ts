f0127f02e5aa6704a1a1960e5c7f6202fb12db86
Patrik Behul <behulpatrik@gmail.com>
2026-05-28 21:02:57 +0200

    Added Background Canvas Effect With Small Randomized Generated Balls.

---
+++ wesiq/static/ts/src/components/background_canvas.ts
@@ -0,0 +1,122 @@
+interface circle {
+    generate():void,
+    animate():void
+}
+
+"use strict"
+
+document.addEventListener("DOMContentLoaded", function():void {
+    // Background Canvas
+
+    // Variables
+
+    const background_canvas:HTMLCanvasElement = document.querySelector(".background_canvas") as HTMLCanvasElement // Gets The Background Canvas
+    const ctx:CanvasRenderingContext2D = background_canvas.getContext("2d") as CanvasRenderingContext2D // Gets The Canvas Context
+
+    const COLORS:string[] = ["#011C6B1A", "#0111421A", "#01258F1A", "#023BE61A", "#0231BD1A"] // Stores The Colors
+    let circles:circle[] = [] // Stores All Circles
+
+    background_canvas.width = window.innerWidth // Sets The Width Of The Background Canvas
+    background_canvas.height = window.innerHeight // Sets The Height Of The Background Canvas
+
+    // Classes
+
+    // Defines The Circle Class
+    class Circle {
+        #radius:number
+        #x:number
+        #y:number
+        #dx:number
+        #dy:number
+        #color:string
+
+        constructor() {
+            this.#radius = Math.floor(generateNumberRange(5, 10)) // Generates Random Radius Of The Circle Between 5 And 10
+            this.#x = Math.floor(generateNumberRange(this.#radius * 2, background_canvas.width - this.#radius * 2)) // Generates Random Position On The Canvas
+            this.#y = Math.floor(generateNumberRange(this.#radius * 2, background_canvas.height - this.#radius * 2)) // Generates Random Position On The Canvas
+            this.#dx = generateNumberRange(-0.5, 0.5) // Sets Random Float Number Of Speed Between -0.5 And 0.5
+            this.#dy = generateNumberRange(-0.5, 0.5) // Sets Random Float Number Of Speed Between -0.5 And 0.5
+            this.#color = COLORS[Math.floor(generateNumberRange(0, COLORS.length - 1))] || "black" // Sets The Random Color
+        }
+
+        // Generates The Circle
+        generate():void {
+            ctx.fillStyle = this.#color
+            ctx.beginPath()
+            ctx.arc(this.#x, this.#y, this.#radius, 0, Math.PI * 2)
+            ctx.fill()
+        }
+
+        // Animates The Circle
+        animate():void {
+            this.#x += this.#dx
+            this.#y += this.#dy
+
+            // Reverses The Direction On The X Axis When Hits One Of The X Borders
+            if(
+                this.#x >= background_canvas.width - this.#radius ||
+                this.#x <= this.#radius
+            ) {
+                this.#dx = -this.#dx
+            }
+
+            // Reverses The Direction On The Y Axis When Hits One Of The Y Borders
+            if(
+                this.#y >= background_canvas.height - this.#radius ||
+                this.#y <= this.#radius
+            ) {
+                this.#dy = -this.#dy
+            }
+        }
+    }
+
+    // Functions
+
+    // Function For Generate Range Of Numbers
+    function generateNumberRange(from:number, to:number):number {
+        return Math.random() * (to - from + 1) + from
+    }
+
+    // Function For Generate The Circles
+    function generateCircles(amount:number = 1):void {
+        for(let i:number = 0; i < amount; i++) {
+            const circle = new Circle() // Creates The Circle
+            circles.push(circle) // Pushes The Circle To The Array Of All Circles
+        }
+    }
+
+    // Function For Display The Circles
+    function displayCircles(array:circle[]):void {
+        for(let i:number = 0; i < array.length; i++) {
+            const circle:circle|undefined = array[i] // Gets The Circle Object
+
+            if(circle) {
+                circle.generate() // Generates The Circle
+                circle.animate() // Animates The Circle
+            }
+        }
+    }
+
+    // Function For Start The Main Animation Loop
+    function initializeMainLoop():void {
+        ctx.clearRect(0, 0, background_canvas.width, background_canvas.height) // Clears The Canvas
+        displayCircles(circles) // Displays The Generated Circles
+        requestAnimationFrame(initializeMainLoop) // Loops The Animation
+    }
+
+    // Events
+
+    // Window Resize Functionality
+    window.addEventListener("resize", function() {
+        background_canvas.width = window.innerWidth // Sets The Width Of The Background Canvas
+        background_canvas.height = window.innerHeight // Sets The Height Of The Background Canvas
+        ctx.clearRect(0, 0, background_canvas.width, background_canvas.height) // Clears The Canvas
+        circles = [] // Deletes The Previous Circles
+        generateCircles(20) // Regenerates 20 Circles
+    })
+
+    // Initialization
+
+    generateCircles(20) // Generates 20 Circles
+    initializeMainLoop() // Initializes The Main Loop
+})
