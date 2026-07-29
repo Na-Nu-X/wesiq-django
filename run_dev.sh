dd14c625c3a80b19f3c83b122c074e64f6cbbc0c
Patrik Behul <behulpatrik@gmail.com>
2026-07-29 10:54:35 +0200

    Added PWA Files To Use The Web As A Mobile App.

--- run_dev.sh
+++ run_dev.sh
@@ -3,19 +3,19 @@
 cd ./wesiq/
 
 # Docker
-gnome-terminal -- bash -c "docker compose up -d; exec bash"
+gnome-terminal --title="Docker" -- bash -c "docker compose up -d; exec bash"
 
 # NPM Watch
-gnome-terminal -- bash -c "npm run watch; exec bash"
+gnome-terminal --title="NPM Watch" -- bash -c "npm run watch; exec bash"
 
 # SASS Watch
-gnome-terminal -- bash -c "npx sass --watch .:.; exec bash"
+gnome-terminal --title="SASS Watch" -- bash -c "npx sass --watch .:.; exec bash"
 
 # Ngrok
-# gnome-terminal -- bash -c "ngrok config add-authtoken 3BiRHm4nENPy49HZVOtJprxcpwj_bG3tnyuBv8sE8PmRNQN5 && ngrok http --domain=delinquently-overdistraught-glynis.ngrok-free.dev 8000; exec bash"
+# gnome-terminal --title="Ngrok" -- bash -c "ngrok config add-authtoken 3BiRHm4nENPy49HZVOtJprxcpwj_bG3tnyuBv8sE8PmRNQN5 && ngrok http --domain=delinquently-overdistraught-glynis.ngrok-free.dev 8000; exec bash"
 
 # # Django Translations
-# gnome-terminal -- bash -c "
+# gnome-terminal --title="Django Translations" -- bash -c "
 # docker compose exec web python manage.py makemessages -l sk -l cs -l en -l es -l fr -l uk -l ru -l pt_BR -l de &&
 # docker compose exec web python manage.py makemessages -d djangojs -l sk -l cs -l en -l es -l fr -l uk -l ru -l pt_BR -l de -e ts -i node_modules &&
 # docker compose exec web python manage.py compilemessages;
--- wesiq/core/settings.py
+++ wesiq/core/settings.py
@@ -32,12 +32,12 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 SECURE_SSL_REDIRECT = os.environ.get('SECURE_SSL_REDIRECT')
 
 # Gmail
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'behulpatrik@gmail.com'
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND")
 
 # ALLOWED_HOSTS = [
 #     "127.0.0.1",
